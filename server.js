90dfe3e4ed50eaeced1953073f85c84c56195ada
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-04-11 11:29:31 +0200

    Added db connection string to server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ import express from "express";
 
 dotenv.config();
 
+const { DB_USER, DB_PASS, DB_HOST, DB_NAME } = process.env;
+const dbConnectionString = `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`;
+
 const app = express();
 const port = 3001;
 
