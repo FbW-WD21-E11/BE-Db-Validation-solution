0b4b8735a8efb4d1441a7f4ff6be7b60f9216585
Franco Speziali <franco.speziali@digitalcareerinstitute.org>
2022-04-11 10:55:39 +0200

    Updated packages, changed to ESM

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
-const cors = require("cors");
-const dotenv = require("dotenv");
-const express = require("express");
-const mongoose = require("mongoose");
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
 
 dotenv.config();
+
 const app = express();
 const port = 3001;
 
 app.use(express.json());
 app.use(cors());
 
-
-
 app.listen(port, () => {
-    console.log(`The server 🙈 is listening on port ${port}`);
+  console.log(`The server 🙈 is listening on port ${port}`);
 });
